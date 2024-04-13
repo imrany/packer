35997f7b1fcf4e1660388de343eaab4eec0739bd
imrany <imranmat254@gmail.com>
2024-04-13 20:31:55 +0300

    checks if config.json exit on the parent directory of the executable file, if not it creates else continues

--- src/main.rs
+++ src/main.rs
@@ -31,10 +31,10 @@ mod launch;
 use launch::launch_browser;
 
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct Configs {
     server_name:String,
-    location:String,
+    location:PathBuf,
     listen:u16
 }
 
@@ -62,40 +62,63 @@ async fn main(){
     let args = Args::parse();
     let port:u16=thread_rng().gen_range(3000..=8080);
 
-    // let current_exe_path=PathBuf::from(current_exe().unwrap());
-    // let configurations_json_file=Path::new(current_exe_path.parent().unwrap()).join("config.json");
-    let configurations_json_file=PathBuf::from("config.json");
-    let configurations:Configs=serde_json::from_str(fs::read_to_string(configurations_json_file).unwrap().as_str()).unwrap(); 
+    let current_exe_path=PathBuf::from(current_exe().unwrap());
+    let mut configurations_json_file_path=PathBuf::from("config.json");
+    println!("{}",configurations_json_file_path.is_file());
+    match configurations_json_file_path.is_file() {
+        true=>&configurations_json_file_path,
+        false=>{
+            configurations_json_file_path=Path::new(current_exe_path.parent().unwrap()).join("config.json");
+            &configurations_json_file_path
+        }
+    };
+    // let configurations_json_file_path=PathBuf::from("config.json");
+    let read_json_content=match fs::read_to_string(&configurations_json_file_path){
+        Ok(v)=>v,
+        Err(_)=>{
+            println!(" Creating config.json...\n ");
+            let data=Configs{
+                server_name:String::from("My web server"),
+                location:PathBuf::from("error"),
+                listen:80
+            };
+            let data_as_json=serde_json::to_string(&data).unwrap();
+            fs::File::create(&configurations_json_file_path).unwrap();
+            fs::write(configurations_json_file_path,&data_as_json).unwrap();
+            data_as_json
+        }
+    };
+    let configurations:Configs=serde_json::from_str(read_json_content.as_str()).unwrap(); 
 
     if let Some(path) = args.root.as_deref() {
-        serve(path.to_string(),port).await;
+        serve(path.into(),port, None).await;
     }
     
     match &args.command {
         Some(Commands::Serve { path }) => {
             if let Some(path) = path.as_deref() {
-                serve(path.to_string(),port).await;
+                serve(path.into(),port, None).await;
             }else {
                 println!("  ERROR Specify a path to serve.");
-                println!(" {}",format!(" HINT: To serve the current folder - 'anvel serve ./'."));
+                println!(" {}",format!(" HINT: To serve the current folder - 'packer serve ./'."));
             }
 
         }
         None => {
-            serve(configurations.location,configurations.listen).await;
+            serve(configurations.clone().location,configurations.listen, Some(&configurations)).await;
         }
     }
 }
 
-async fn serve(path: String, port:u16) {
+async fn serve(path: PathBuf, port:u16, configurations:Option<&Configs>) {
     let ipv4: (Ipv4Addr, u16)=("0.0.0.0".parse().unwrap(),port);
     let server=HttpServer::new(move ||
         App::new()
-            .service(Files::new("/", path.clone()).show_files_listing().index_file("index.html")
+            .service(Files::new("/", &path).show_files_listing().index_file("index.html")
                 .default_handler(fn_service(|req: ServiceRequest| async {
                     let (req, _) = req.into_parts();
                     let current_exe_path=PathBuf::from(current_exe().unwrap());
-                    let file = NamedFile::open_async(Path::new(current_exe_path.parent().unwrap()).join("assets/error_page.html")).await?;
+                    let file = NamedFile::open_async(Path::new(current_exe_path.parent().unwrap()).join("error/index.html")).await?;
                     let res = file.into_response(&req);
                     Ok(ServiceResponse::new(req, res))
                 }))
@@ -108,11 +131,13 @@ async fn serve(path: String, port:u16) {
             match local_ip(){
                 Ok(ip) => {
                     url=format!("http://{ip}:{port}");
+                    println!(" Server name: {} ", configurations.unwrap().server_name);
                     println!(" Networkhost: {ip} ");
                     println!(" Url: {url} ");
                 },
                 Err(_) => {
                     url=format!("http://localhost:{port}");
+                    println!(" Server name: {} ", configurations.unwrap().server_name);
                     println!(" Localhost: localhost ");
                     println!(" Url: {url}");
                 }
